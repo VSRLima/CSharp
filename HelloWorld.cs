2a7467786771a3bf5e5daf3ca2d4ab6695296337
VSRLima <vinisrlima@hotmail.com>
2021-07-13 20:53:50 -0300

    first commit

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloWorldCSharp
+{
+    public class HelloWorld
+    {
+        string _hello;
+
+        public void SetHello (string hello)
+        {
+            _hello = hello;
+        }
+
+        public string GetHello()
+        {
+            return _hello;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace learningCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           HelloWorldCSharp.HelloWorld hello = new HelloWorldCSharp.HelloWorld();
+           hello.SetHello("Hello World");
+           Console.WriteLine(hello.GetHello());
+        }
+    }
+}
